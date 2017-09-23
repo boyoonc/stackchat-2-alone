98816bf1b2ccca46eb6bbcf050bf6155aa918d4c
BOYOON CHOI <boyoonchoi@BOYOONs-alberto-MacBook-Pro.local>
2017-09-23 14:46:36 -0400

    all works except bonus for channelname

--- client/components/MessagesList.js
+++ client/components/MessagesList.js
@@ -1,7 +1,70 @@
+// import React, { Component } from "react";
+// import Message from "./Message";
+// import NewMessageEntry from "./NewMessageEntry";
+// import {changeCurrentChannel} from "../store";
+// import { connect } from "react-redux";
+
+// function Messages(props) {
+//   const channelId = Number(props.channelId); // because it's a string "1", not a number!
+//   const filteredMessages = props.messages.filter(
+//     message => message.channelId === channelId
+//   );
+
+//   return (
+//     <div>
+//       <ul className="media-list">
+//         {filteredMessages.map(message => (
+//           <Message message={message} key={message.id} />
+//         ))}
+//       </ul>
+//       <NewMessageEntry channelId={channelId} />
+//     </div>
+//   );
+// }
+
+// class MessagesListLoader extends Component{
+//   componentDidMount () {
+//     this.props.changeCurrentChannel(this.props.channel.name);
+//   }
+
+//   componentWillReceiveProps (nextProps) {
+//     if (nextProps.channel.name !== this.props.channel.name) {
+//       this.props.changeCurrentChannel(nextProps.channel.name);
+//     }
+//   }
+
+//   render () {
+//     return (
+//       <Messages {...this.props} />
+//     );
+//   }
+// }
+
+// function mapStateToProps(state, ownProps){
+//   return {
+//     // channelId: ownProps.match.params.channelId,
+//     channel: state.channels.find(channel => channel.id === channelId) || { name: '' },
+//     messages: state.messages
+//   }
+// }
+
+// const mapDispatchToProps = function (dispatch) {
+//   return {
+//     changeCurrentChannel(channelName) {
+//       dispatch(changeCurrentChannel(channelName));
+//     }
+//   };
+// };
+
+
+// const container = connect(mapStateToProps, mapDispatchToProps)(MessagesListLoader)
+
+// export default container;
+
 import React, { Component } from "react";
 import Message from "./Message";
 import NewMessageEntry from "./NewMessageEntry";
-import {changeCurrentChannel} from "../store";
+import store from "../store";
 import { connect } from "react-redux";
 
 function Messages(props) {
@@ -9,7 +72,6 @@ function Messages(props) {
   const filteredMessages = props.messages.filter(
     message => message.channelId === channelId
   );
-
   return (
     <div>
       <ul className="media-list">
@@ -22,41 +84,13 @@ function Messages(props) {
   );
 }
 
-class MessagesListLoader extends Component{
-  componentDidMount () {
-    this.props.changeCurrentChannel(this.props.channel.name);
-  }
-
-  componentWillReceiveProps (nextProps) {
-    if (nextProps.channel.name !== this.props.channel.name) {
-      this.props.changeCurrentChannel(nextProps.channel.name);
-    }
-  }
-
-  render () {
-    return (
-      <Messages {...this.props} />
-    );
-  }
-}
-
 function mapStateToProps(state, ownProps){
   return {
-    // channelId: ownProps.match.params.channelId,
-    channel: state.channels.find(channel => channel.id === channelId) || { name: '' },
+    channelId: ownProps.match.params.channelId,
     messages: state.messages
   }
 }
 
-const mapDispatchToProps = function (dispatch) {
-  return {
-    changeCurrentChannel(channelName) {
-      dispatch(changeCurrentChannel(channelName));
-    }
-  };
-};
-
-
-const container = connect(mapStateToProps, mapDispatchToProps)(MessagesListLoader)
+const container = connect(mapStateToProps)(Messages)
 
 export default container;
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,24 +1,39 @@
+// import React, { Component } from 'react';
+// import NameEntry from './NameEntry';
+// import { connect } from 'react-redux'
+
+// function Navbar (props) {
+
+// 	const {currentChannel} = props
+
+//     return (
+//       <nav>
+//         <h3>{props.currentChannel}</h3>
+//         <NameEntry />
+//       </nav>
+//     );
+
+// }
+
+// const mapStateToProps = function(state){
+// 	return {
+// 		currentChannel: state.currentChannel
+// 	}
+// }
+
+// export default connect (mapStateToProps)(Navbar)
+
 import React, { Component } from 'react';
 import NameEntry from './NameEntry';
-import { connect } from 'react-redux'
 
-function Navbar (props) {
+export default class Navbar extends Component {
 
-	const {currentChannel} = props
-
+  render () {
     return (
       <nav>
-        <h3>{props.currentChannel}</h3>
+        <h3># channelname goes here</h3>
         <NameEntry />
       </nav>
     );
-
+  }
 }
-
-const mapStateToProps = function(state){
-	return {
-		currentChannel: state.currentChannel
-	}
-}
-
-export default connect (mapStateToProps)(Navbar)
